56090dc0cf3d66a8d60fdc3ac9a4238db936cd1c
hadesong <hadesong@gmail.com>
2016-05-30 23:43:03 +0800

    添加404

--- app_package/view.py
+++ app_package/view.py
@@ -9,6 +9,9 @@ def index():
     a = app.config['TEST']
     return render_template('index.html')
 
+@app.errorhandler(404)
+def page_not_found(error):
+    return  render_template('404.html') , 404
 
 '''
 @app.route('/search_city' , methods =['POST' ])   
